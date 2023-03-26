79fe576a1d83b905f50780668d1212792f9957a0
wKategianes <wKategianes@gmail.com>
2023-03-26 00:41:10 -0700

    Initial Concept

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import './NavBar.css';
+import logo from '../../images/pokeball.png';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -8,13 +10,14 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
+    <nav class="navBar">
+      <div class="logo"><img src={logo} alt="pokeball logo"/></div>
+      <Link class="navItem" to="/orders">Order History</Link>
       &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
+      <Link class="navItem" to="/home">Home</Link>
       &nbsp;&nbsp;
       <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      &nbsp;&nbsp;<Link class="navItem" to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import kantoMap from '../../images/kantoMap.png';
+
+export default function NewOrderPage() {
+  return (
+    <>
+      <div>
+        <mig src={kantoMap} />
+      </div>
+    </>
+  );
+}
